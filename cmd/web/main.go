b9c5e866e8af67c75cded2126c4155225157b5a5
Sam Maton <sam.maton91@gmail.com>
2026-01-10 09:18:15 +0000

    added structured logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"flag"
-	"log"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", getVideoFeed)
 	mux.HandleFunc("/feed", getVideoFeed)
 	mux.HandleFunc("GET /videos/{id}", getVideoById)
 
-	log.Printf("starting server on http://localhost%s/", *addr)
+	logger.Info(fmt.Sprintf("starting server on http://localhost%s/", *addr))
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
